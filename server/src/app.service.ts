991dc552d8507a89693cf400332441e4de4ea265
Roman <romadzao@gmail.com>
2021-02-06 16:11:13 -0500

    Change app service to import external file using tsconfig path

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { hello } from '@shared/hello'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return hello()
   }
 }
---
+++ shared/hello.ts
@@ -0,0 +1,1 @@
+export const hello = () => "this won't appear in browser"
